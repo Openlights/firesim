3040d400d7f81d638fb71067b213ec6700b8776c
Jon Evans <jon@craftyjon.com>
2013-01-18 16:27:08 -0500

    Highlight fixture on hover

--- ui/fixturedeclarative.py
+++ ui/fixturedeclarative.py
@@ -8,21 +8,40 @@ class FixtureDeclarative(QtDeclarative.QDeclarativeItem):
         super(FixtureDeclarative, self).__init__(parent)
         self.setFlag(QtGui.QGraphicsItem.ItemHasNoContents, False)
         self.setAcceptedMouseButtons(QtCore.Qt.MouseButton.LeftButton)
+        self.setAcceptsHoverEvents(True)
         self.color = QtGui.QColor(100, 100, 100)
-        self.setHeight(4)
+        self.setHeight(16)
         self.setWidth(128)
         self.dragging = False
+        self.hovering = False
+        self.drag_start = None
+        self.rect = QtCore.QRect(0, 0, 128, 16)
 
     def paint(self, painter, options, widget):
-        self.rect = QtCore.QRect(0, 0, self.width(), self.height())
-        painter.fillRect(self.rect, QtGui.QColor(200, 200, 0))
+        if self.hovering:
+            painter.setPen(QtGui.QColor(50, 100, 255, 255))
+            painter.setBrush(QtGui.QColor(50, 100, 255, 255))
+            hover_rect = QtCore.QRect(2, 4, 124, 8)
+            painter.drawRoundRect(hover_rect, 16, 16)
+        fixture_bg = QtCore.QRect(4, 6, 120, 4)
+        painter.fillRect(fixture_bg, QtGui.QColor(200, 200, 0))
+
+    def hoverEnterEvent(self, event):
+        self.hovering = True
+        self.update()
+
+    def hoverLeaveEvent(self, event):
+        self.hovering = False
+        self.update()
 
     def mouseMoveEvent(self, event):
         if self.dragging:
-            self.setPos(event.scenePos())
+            self.setPos(event.scenePos() - self.drag_start - self.pos())
 
     def mousePressEvent(self, event):
         self.dragging = True
+        self.drag_start = event.scenePos()
 
     def mouseReleaseEvent(self, event):
         self.dragging = False
+        self.drag_start = None
